f817537bdddf237a4b259da4f1d9a383c4e75b28
CarlaBia <cal.beatriz@hotmail.com>
2022-09-14 20:41:35 -0300

    Projeto Criado

---
+++ src/test/java/com/carlabeatriz/cursoWeb/CursoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlabeatriz.cursoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
