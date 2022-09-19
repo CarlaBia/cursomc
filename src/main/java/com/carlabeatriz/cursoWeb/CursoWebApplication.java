41ac9e45026104847a98eafb4c296b65e74eb4c5
Carla Beatriz <cal.beatriz@hotmail.com>
2022-09-19 15:07:57 -0300

    Produto e associação de muitos para muitos

--- src/main/java/com/carlabeatriz/cursoWeb/CursoWebApplication.java
+++ src/main/java/com/carlabeatriz/cursoWeb/CursoWebApplication.java
@@ -8,25 +8,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.carlabeatriz.cursoWeb.domain.Categoria;
+import com.carlabeatriz.cursoWeb.domain.Produto;
 import com.carlabeatriz.cursoWeb.repositories.CategoriaRepository;
+import com.carlabeatriz.cursoWeb.repositories.ProtudoRepository;
 
 @SpringBootApplication
-public class CursoWebApplication implements CommandLineRunner{
-	
+public class CursoWebApplication implements CommandLineRunner {
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+	@Autowired
+	private ProtudoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursoWebApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
