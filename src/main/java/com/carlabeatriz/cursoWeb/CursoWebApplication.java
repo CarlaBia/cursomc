7b9563afd62a970dd7eea32eb3f3138a8b7e3bb3
Carla Beatriz <cal.beatriz@hotmail.com>
2022-09-19 11:26:55 -0300

    Criando operação de instanciação

--- src/main/java/com/carlabeatriz/cursoWeb/CursoWebApplication.java
+++ src/main/java/com/carlabeatriz/cursoWeb/CursoWebApplication.java
@@ -1,13 +1,32 @@
 package com.carlabeatriz.cursoWeb;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoWebApplication {
+import com.carlabeatriz.cursoWeb.domain.Categoria;
+import com.carlabeatriz.cursoWeb.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoWebApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoWebApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
