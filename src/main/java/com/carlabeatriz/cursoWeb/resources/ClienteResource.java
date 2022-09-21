254cfcad1fa5543fe9bdb93171dec1723bbf4b3c
Carla Beatriz <cal.beatriz@hotmail.com>
2022-09-21 10:07:14 -0300

    Endpoint/clientes/{id} disponível

--- src/main/java/com/carlabeatriz/cursoWeb/resources/ClienteResource.java
+++ src/main/java/com/carlabeatriz/cursoWeb/resources/ClienteResource.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.carlabeatriz.cursoWeb.domain.Categoria;
-import com.carlabeatriz.cursoWeb.services.CategoriaService;
+import com.carlabeatriz.cursoWeb.domain.Cliente;
+import com.carlabeatriz.cursoWeb.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/categorias")
-public class CategoriaResource {
+@RequestMapping(value="/clientes")
+public class ClienteResource {
 
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find (@PathVariable Integer id) {
-		Categoria obj = service.find(id);
+		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
 	
