9915e67958329136f75afe66898168e39851d313
Carla Beatriz <cal.beatriz@hotmail.com>
2022-09-21 13:01:43 -0300

    Pedido, estadoPagamento, pagamento

--- src/main/java/com/carlabeatriz/cursoWeb/CursoWebApplication.java
+++ src/main/java/com/carlabeatriz/cursoWeb/CursoWebApplication.java
@@ -1,5 +1,6 @@
 package com.carlabeatriz.cursoWeb;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.carlabeatriz.cursoWeb.domain.Cidade;
 import com.carlabeatriz.cursoWeb.domain.Cliente;
 import com.carlabeatriz.cursoWeb.domain.Endereco;
 import com.carlabeatriz.cursoWeb.domain.Estado;
+import com.carlabeatriz.cursoWeb.domain.Pagamento;
+import com.carlabeatriz.cursoWeb.domain.PagamentoComBoleto;
+import com.carlabeatriz.cursoWeb.domain.PagamentoComCartao;
+import com.carlabeatriz.cursoWeb.domain.Pedido;
 import com.carlabeatriz.cursoWeb.domain.Produto;
+import com.carlabeatriz.cursoWeb.domain.enums.EstadoPagamento;
 import com.carlabeatriz.cursoWeb.domain.enums.TipoCliente;
 import com.carlabeatriz.cursoWeb.repositories.CategoriaRepository;
 import com.carlabeatriz.cursoWeb.repositories.CidadeRepository;
 import com.carlabeatriz.cursoWeb.repositories.ClienteRepository;
 import com.carlabeatriz.cursoWeb.repositories.EnderecoRepository;
 import com.carlabeatriz.cursoWeb.repositories.EstadoRepository;
+import com.carlabeatriz.cursoWeb.repositories.PagamentoRepository;
+import com.carlabeatriz.cursoWeb.repositories.PedidoRepository;
 import com.carlabeatriz.cursoWeb.repositories.ProtudoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursoWebApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoWebApplication.class, args);
@@ -57,35 +69,51 @@ public class CursoWebApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
-		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
+
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
-		
-		estadoRepository.saveAll(Arrays.asList(est1,est2));
-		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
-		
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
 		Cliente cli1 = new Cliente(null, "Maria", "maria@hotmail.com", "44445678", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("12345-1234", "321432-765"));
-		
+
 		Endereco e1 = new Endereco(null, "Bua boa Viagem", "30", "Térreo", "Bela Vista", "44444440", cli1, c1);
 		Endereco e2 = new Endereco(null, "Rua jardins", "3", "Térreo", "Flores", "999899908", cli1, c2);
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-		
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("21/09/2022 11:55"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("22/09/2022 12:00"), cli1, e2);
+
 		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("22/09/2022 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
-	
 
 }
