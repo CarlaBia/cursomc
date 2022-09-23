159db2f9fc36261051c50dc6defe3ec28b616aec
Carla Beatriz <cal.beatriz@hotmail.com>
2022-09-23 12:13:05 -0300

    Deletando uma Categoria e criando uma mensagem de erro personalizada

--- src/main/java/com/carlabeatriz/cursoWeb/services/CategoriaService.java
+++ src/main/java/com/carlabeatriz/cursoWeb/services/CategoriaService.java
@@ -3,30 +3,44 @@ package com.carlabeatriz.cursoWeb.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.carlabeatriz.cursoWeb.domain.Categoria;
 import com.carlabeatriz.cursoWeb.repositories.CategoriaRepository;
+import com.carlabeatriz.cursoWeb.services.exeptions.DataIntegrityException;
 import com.carlabeatriz.cursoWeb.services.exeptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow (() -> new ObjectNotFoundException(
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-				}
-	public Categoria insert (Categoria obj) {
+	}
+
+	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
-	public Categoria update (Categoria obj) {
+
+	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
 	}
 }
